0727bef2359b4ec6d32c9c736e432c7a2505b863
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-07-02 12:44:42 -0300

    Initial front-end (React) app scaffold

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Basic mobile-responsive layout
+export default function App() {
+  return (
+    <div style={{ maxWidth: 500, margin: 'auto', padding: 20 }}>
+      <h1>Rehearsal Scheduler</h1>
+      <p>Plan and track your band/choir rehearsals</p>
+      {/* Add login, calendar, attendance, reminders features here */}
+    </div>
+  );
+}
